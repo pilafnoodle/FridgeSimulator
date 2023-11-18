276de1ba60002212076c425f855ffe5c275c1509
pilafnoodle <amberlin618@gmail.com>
2023-11-18 11:51:07 -0800

    updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,60 +1,70 @@
-#include "food.cpp"
-#include "storage.cpp"
+#include "food.h"
+#include "storage.h"
 #include <iostream>
+#include <stdio.h>
+using namespace std;
 
 void putNewItemsInStorage(Storage s){
-    // cout<<"You are putting items inside your "<<s.getName();<<endl;
-    // string name;
-    // string category;
-    // while(input!="q"){
-    //     cout<<"enter name of item(q to quit): "<<endl;
-    //     cin>>name;
-    //     if(name="q"){
-    //         break;
-    //     }
-    //     cout<<"enter category (v for vegetables, d for dairy, m for meat, g for grains and p for preserved food)"<<endl;
-    //     cin>>category;
-    //     RawFood newItem(name,category);
-    //     s.addItem(newItem);      
-    // }
+    cout<<"You are putting items inside your "<<s.getName()<<endl;
+    string foodName;
+    string input;
+    int category;
+    while(input!="q"){
+        cout<<"enter name of item(q to quit): "<<endl;
+        cin>>foodName;
+        if(foodName=="q"){
+            break;
+        }
+        cout<<"enter category (1 for vegetables, 2 for dairy, 3 for meat, 4 for grains and 5 for preserved food)"<<endl;
+        cin>>category;
+        // RawFood newItem(foodName,category);
+        // s.addFood(newItem);      
+    }
 }
 
 void printMenu(){
     cout<<"c: lets cook something baby"<<endl;
     cout<<"l: look at storage"<<endl;
     cout<<"s: go to bed and end the day"<<endl;
-
-
 }
 
 
 int main(){
-    int daysPassed=0;
-    Storage freezer;
-    Storage pantry;
-    Storage fridge;
-    vector<storage> containers={fridge,freezer,pantry};
 
+    int daysPassed=0;
+    Storage freezer("Freezer",10,0.0);
+    Storage pantry("Pantry",20,5.0);
+    Storage fridge("Fridge",8,2.0);
+    
+
+    vector<Storage> container={fridge,freezer,pantry};
+    putNewItemsInStorage(s);//ask if user wants to put anything in storage
+
+
+    return 0;
+    bool endOfDay=false;
     while(daysPassed<365){
     
-        showStatusOfFridge();
-        for(/*for each storage item s*/){
-            putNewItemsInStorage(Storage s);//ask if user wants to put anything in storage
+        // showStatusOfContainers();
+        for(Storage s:container){
+            putNewItemsInStorage(s);//ask if user wants to put anything in storage
             printMenu();
             char input;
             cin>>input;
             switch(input){
                 case 'c':
-                    cookFood();
+                    // cookFood();
                 case 'l':
-                    lookAtStorage();
+                    // lookAtStorage();
                 case 's':
-                    daysPassed++;
-                    incrementEachDaysInFridge();
-                    break;
+                    endOfDay=true;
+                    
             }   
         }
+        // incrementEachDaysInFridge();
+        daysPassed++;
     }
+    return 0;
 }
 
 
