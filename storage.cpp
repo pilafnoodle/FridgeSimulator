a69bf987c2ae8aa9d8275d913b8147906e3bf112
pilafnoodle <amberlin618@gmail.com>
2023-11-18 11:47:50 -0800

    updated storage files

--- storage.cpp
+++ storage.cpp
@@ -1,1 +1,10 @@
+#include "Storage.h"
+string Storage::getName(){
+    return name;
+}
 
+Storage::Storage(string name,int capacity, double spoilRate){
+    this->name=name;
+    this->capacity=capacity;
+    this->spoilRate=spoilRate;
+}
--- storage.h
+++ storage.h
@@ -1,13 +1,26 @@
+#ifndef _STORAGE_H_
+#define _STORAGE_H_
+
 #include "food.h"
 #include <vector>
+#include <string>
+
+
+using namespace std;
 
 class Storage {
     public:
-        Storage(int capacity, double spoilRate);
-        void addFood(); // Need to add parameter from food file
+        Storage(string name,int capacity, double spoilRate);
+        void addFood(Food newItem); // Need to add parameter from food file
+        void removeFood(string name); // Need to add parameter from food file
+        string getName();
+
     private:
+        string name;
         int capacity;
         double spoilRate;
         std::vector<Food> items;
 
 };
+
+#endif
