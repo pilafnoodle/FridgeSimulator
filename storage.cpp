21090e17dbf40872f00b2757d810ea5d947793bb
Sweden Agunenye <swedenagu@ucr-secure-47-10-13-237-59.wnet.ucr.edu>
2023-11-18 14:23:44 -0800

    cleaning up code

--- storage.cpp
+++ storage.cpp
@@ -1,3 +1,34 @@
+#include <iostream>
+#include "storage.h"
+
+Storage::Storage(string name, int capacity, double spoilRate)
+{
+    // Storage class::Constructor(capacity, spoilRate)
+    this->name = name;
+    this->capacity = capacity;
+    this->spoilRate = spoilRate;
+}
+
+string Storage::getName() {
+    return name;
+}
+
+void Storage::addFood(Food food)
+{
+    // Doesn't exceed capacity
+    if (items.size() < capacity)
+    {
+        items.push_back(food);
+        // std::cout << "New addition: " << food.getItem();
+        // Exceeds capacity
+    }
+
+    else
+    {
+        std::cout << "There is no space in the storage.";
+    }
+}
+
 /*void Storage::displayContents() const {
     std::cout << "Storage Items:\n":
     for()
