680f50d685817ada42cd7c2e5b56fbb713976004
Sweden Agunenye <swedenagu@ucr-secure-47-10-13-237-59.wnet.ucr.edu>
2023-11-18 11:23:12 -0800

    added comments to classes

--- food.cpp
+++ food.cpp
@@ -3,6 +3,8 @@
 #include "food.h"
 #include <vector>
 using namespace std;
+
+// food class with name of food and lifetime in fridge
 class Food
 {
     protected:
@@ -16,12 +18,14 @@ class Food
         }
 };
 
+// raw food class that keeps track of the number of days in the fridge
 class RawFood: public Food
 {
     public:
         int daysInFridge;
 };
 
+// cooked food class with a vector array of ingredients
 class CookedFood: public Food
 {
     public:
