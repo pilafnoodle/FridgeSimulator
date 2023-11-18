45e35bba14103d0f658e3c782c6ce89e9576743b
Sweden Agunenye <swedenagu@ucr-secure-47-10-13-237-59.wnet.ucr.edu>
2023-11-18 11:59:32 -0800

    updating food

--- food.cpp
+++ food.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <string>
 #include "food.h"
 #include <vector>
@@ -7,15 +6,17 @@ using namespace std;
 // food class with name of food and lifetime in fridge
 class Food
 {
-    protected:
+    private:
         string name;
         int lifetime;
     public:
+        Food() {}
         Food(string n, int t)
         {
             name = n;
             lifetime = t;
         }
+        int get
 };
 
 // raw food class that keeps track of the number of days in the fridge
